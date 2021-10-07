13e3eaf49b2f70b74da97dda7674338ba5377850
Angel Ali <77410960+Angel-Ali@users.noreply.github.com>
2021-10-07 16:51:33 -0500

    Add files via upload

---
+++ src/components/about.jsx
@@ -0,0 +1,5 @@
+const About = () => {
+    return (<h4>about me Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui reiciendis, cumque exercitationem cupiditate quos officiis magnam quam voluptatem nostrum hic nisi blanditiis accusamus maxime aliquid natus tempora recusandae suscipit placeat.</h4>);
+}
+ 
+export default About;
